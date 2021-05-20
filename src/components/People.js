93781507d5d6490ddf528d71fccad33b723e0163
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-05-20 23:08:56 +0430

    useQuery Hook

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./Person"
+
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
 
 const People = () => {
+  const { data, status } = useQuery("people", fetchPeople);
   return (
     <div>
       <h2>People</h2>
+      {status === "error" && <div>Error Fetching Data</div>}
+      {status === "loading" && <div>Loading Fetching Data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
